ab876e5d4c2800d9fd375e421d0fe146ae04546d
dedoyle <dedoyle@163.com>
2022-01-23 20:56:26 +0800

    remove api

--- services/postService.ts
+++
@@ -1,7 +0,0 @@
-import { get } from './apiClient'
-import { PostListData } from 'pages/api/post_list'
-
-export const getPostList = async (): Promise<PostListData> => {
-  const res = await get<PostListData>('post_list')
-  return res
-}
