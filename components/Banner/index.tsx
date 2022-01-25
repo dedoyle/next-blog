c5f37d68ba731eb568cc0e6111bec76bd352cbee
dedoyle <dedoyle@163.com>
2022-01-25 22:18:38 +0800

    comment image

--- components/Banner/index.tsx
+++ components/Banner/index.tsx
@@ -1,11 +1,14 @@
-import banner1 from "./imgs/20210531-barnimages-1.jpg";
-// import banner2 from "./imgs/20210608-barnimages-2.jpg";
-// import banner4 from "./imgs/20210622-barnimages-4.jpg";
-// import banner10 from "./imgs/20210304-barnimages-10.jpg";
-import Image from "next/image";
+// import banner1 from "./imgs/20210531-barnimages-1.jpg";
+// // import banner2 from "./imgs/20210608-barnimages-2.jpg";
+// // import banner4 from "./imgs/20210622-barnimages-4.jpg";
+// // import banner10 from "./imgs/20210304-barnimages-10.jpg";
+// import Image from "next/image";
 
 const Banner = () => {
-  return <Image src={banner1} alt="banner" />;
+  // return <Image src={banner1} alt="banner" />;
+  return (
+    <h1>技术博客</h1>
+  )
 };
 
 export default Banner;
