160401fa95c00c14debe77d7c1adaf231eae9c29
dedoyle <dedoyle@163.com>
2022-01-25 22:01:55 +0800

    add banner image

--- components/Banner/index.tsx
+++ components/Banner/index.tsx
@@ -1,5 +1,11 @@
+import banner1 from "./imgs/20210531-barnimages-1.jpg";
+// import banner2 from "./imgs/20210608-barnimages-2.jpg";
+// import banner4 from "./imgs/20210622-barnimages-4.jpg";
+// import banner10 from "./imgs/20210304-barnimages-10.jpg";
+import Image from "next/image";
+
 const Banner = () => {
-  return <h1>技术博客</h1>;
+  return <Image src={banner1} alt="banner" />;
 };
 
 export default Banner;
